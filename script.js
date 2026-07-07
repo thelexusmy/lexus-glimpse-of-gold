735430f909b1daf1c52ce96d901e982a39263560
thelexusmy <thelexusmy@gmail.com>
2026-07-07 17:25:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Lexus Glimpse of Gold loaded.");
+function verifyNRIC(){
+
+    let nric = document.getElementById("nric").value;
+
+    let error = document.getElementById("error");
+
+
+    if(!/^\d{12}$/.test(nric)){
+
+        error.innerHTML =
+        "Please enter a valid 12-digit NRIC number.";
+
+        return;
+
+    }
+
+
+    window.location.href = "register.html";
+
+}
